7593902a85d8802654c716225c8b3d58ef51bc9d
Jon Gjengset <jon@thesquareplanet.com>
2017-10-02 16:43:48 -0400

    rustfmt

--- src/flow/migrate/sharding.rs
+++ src/flow/migrate/sharding.rs
@@ -86,10 +86,15 @@ pub fn shard(
             // non-internal nodes are always pass-through
             HashMap::new()
         };
-        if need_sharding.is_empty() &&
-            (input_shardings.len() == 1 || input_shardings.iter().all(|(_, s)| s.is_none()))
+        if need_sharding.is_empty()
+            && (input_shardings.len() == 1 || input_shardings.iter().all(|(_, s)| s.is_none()))
         {
-            let mut s = input_shardings.iter().map(|(_, s)| s).cloned().next().unwrap();
+            let mut s = input_shardings
+                .iter()
+                .map(|(_, s)| s)
+                .cloned()
+                .next()
+                .unwrap();
             if input_shardings
                 .iter()
                 .any(|(_, s)| *s == Sharding::ForcedNone)
